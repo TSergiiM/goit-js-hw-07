d2a70df415e289a3aadfa26b4057d43b20e9086e
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-12 22:49:04 +0200

    Add-EscKey-to-gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,16 +25,27 @@ function createGalleryMarkup(galleryItems) {
     })
     .join('');
 }
-
 refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
 const onGalleryClick = event => {
   event.preventDefault();
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-  const instance = basicLightbox.create(`
-<img src='${event.target.dataset.source}'>`);
+  const instance = basicLightbox.create(`<img src='${event.target.dataset.source}'>`, {
+    onShow: instance => {
+      document.addEventListener('keydown', onEsc);
+    },
+    onClose: instance => {
+      document.removeEventListener('keydown', onEsc);
+    },
+  });
 
+  function onEsc(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
   instance.show();
 };
 refs.gallery.addEventListener('click', onGalleryClick);
