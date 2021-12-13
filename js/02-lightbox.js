fc7ee4e0c2bb482212e55bfa938e39da212d952c
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-13 20:44:06 +0200

    Create-markup-task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,45 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>`;
+    })
+    .join('');
+}
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// const onGalleryClick = event => {
+//   event.preventDefault();
+//   if (!event.target.classList.contains('gallery__image')) {
+//     return;
+//   }
+//   const instance = basicLightbox.create(`<img src='${event.target.dataset.source}'>`, {
+//     onShow: instance => {
+//       document.addEventListener('keydown', onEsc);
+//     },
+//     onClose: instance => {
+//       document.removeEventListener('keydown', onEsc);
+//     },
+//   });
+
+//   function onEsc(event) {
+//     if (event.code === 'Escape') {
+//       instance.close();
+//     }
+//   }
+//   instance.show();
+// };
+// refs.gallery.addEventListener('click', onGalleryClick);
